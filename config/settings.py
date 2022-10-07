1bf285e94d2ebf37cb0d2d59191381d41d74baad
JordiBanuelos99 <banuelos99@DESKTOP-SBJALIQ.localdomain>
2022-10-06 18:04:07 -0700

    initial login

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,10 @@ STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
