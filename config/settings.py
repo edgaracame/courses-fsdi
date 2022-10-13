1aef6999b5e10996ada8915098c0dabb9c42fcd1
JordiBanuelos99 <banuelosmendez99@gmail.com>
2022-10-13 00:19:51 -0700

    Password reset Email

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,12 @@ LOGOUT_REDIRECT_URL = "home"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#SMTP Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dystauth2022@gmail.com'
+EMAIL_HOST_PASSWORD = 'pzhirtzbuxdymgit'
+DEFAULT_FROM_EMAIL = 'Dyst Team <noreply@dystauth.com>'
