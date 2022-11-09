4c4a4e209b83a913055ad896b78709c86bb31d80
Edgar Castillo <edgar101999.eacm@gmail.com>
2022-11-08 18:09:26 -0800

    Version 1.1

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,10 +7,15 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_sent.html"), name="password_reset_done"),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_form.html"), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_done.html"), name="password_reset_complete"),
-    path('password/', PasswordsChangeView.as_view(template_name="registration/password.html"), name="password"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(
+        template_name="registration/password_reset.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(
+        template_name="registration/password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(
+        template_name="registration/password_reset_form.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name="registration/password_reset_done.html"), name="password_reset_complete"),
+    path('password/', PasswordsChangeView.as_view(
+        template_name="registration/password.html"), name="password"),
     path('password_success', views.password_success, name="password_success"),
 ]
